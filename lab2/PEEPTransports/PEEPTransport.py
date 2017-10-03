6a83c5c2b2bcf4ae5f05541ce49caafaf6297e1e
MenghanBai <31711820+MenghanBai@users.noreply.github.com>
2017-10-03 14:44:03 -0400

    update PEEPTransport

--- lab2/PEEPTransports/PEEPTransport.py
+++ lab2/PEEPTransports/PEEPTransport.py
@@ -9,9 +9,22 @@ class PEEPTransport(StackingTransport):
     def write(self, data):
         if self.protocol:
             print("PEEPTransport: Write got {} bytes of data to package".format(len(data)))
-            # Currently no chunking
-            pkt = PEEPPacket.makeDataPacket(self.protocol.raisedSeqNum(len(data)), data)
-            super().write(pkt.__serialize__())
+            # Currently add chunking
+            i = 0
+            n = 0
+            self.chunk = list()
+            while(i < len(data)):
+                if i + 1024 > len(data):
+                    self.chunk[n]=data[i:i+1024]
+                    i = i + 1024
+                    n = n + 1
+                    pkt = PEEPPacket.makeDataPacket(self.protocol.raisedSeqNum(len(chunk[n])), chunk[n])
+                    super().write(pkt.__serialize__())
+                else:
+                    self.chunk[n]=data[i]
+                    pkt = PEEPPacket.makeDataPacket(self.protocol.raisedSeqNum(len(chunk[n])), chunk[n])
+                    super().write(pkt.__serialize__())
+
         else:
             print("PEEPTransport: Undefined protocol, writing anyway...")
             print("PEEPTransport: Write got {} bytes of data to pass to lower layer".format(len(data)))
