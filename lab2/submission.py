3965c8d15350490f481d423eca1ff628dfc568a5
Te Meng <tmeng4@jhu.edu>
2017-10-04 17:12:30 -0400

    modularization

--- lab2/submission.py
+++ lab2/submission.py
@@ -40,15 +40,16 @@ if __name__ == "__main__":
         remoteAddress = testArgs[1]
     loop = asyncio.get_event_loop()
     loop.set_debug(enabled=True)
-    f_server = StackingProtocolFactory(lambda: PassThroughLayer1(), lambda: ServerProtocol(loop))
-    f_client = StackingProtocolFactory(lambda: PassThroughLayer2(), lambda: ClientProtocol(loop))
-    ptConnector_server = playground.Connector(protocolStack=f_server)
-    ptConnector_client = playground.Connector(protocolStack=f_client)
-    playground.setConnector("server", ptConnector_server)
-    playground.setConnector("client", ptConnector_client)
+    # f_server = StackingProtocolFactory(lambda: PassThroughLayer1(), lambda: ServerProtocol(loop))
+    # f_client = StackingProtocolFactory(lambda: PassThroughLayer2(), lambda: ClientProtocol(loop))
+    # ptConnector_server = playground.Connector(protocolStack=f_server)
+    # ptConnector_client = playground.Connector(protocolStack=f_client)
+    # playground.setConnector("server", ptConnector_server)
+    # playground.setConnector("client", ptConnector_client)
 
     if mode.lower() == "server":
-        coro = playground.getConnector('server').create_playground_server(lambda: EchoServerProtocol(), 101)
+        # coro = playground.getConnector('server').create_playground_server(lambda: EchoServerProtocol(), 101)
+        coro = playground.getConnector('lab2_protocol').create_playground_server(lambda: EchoServerProtocol(), 101)
         server = loop.run_until_complete(coro)
         print("Submission: Server started at {}".format(server.sockets[0].gethostname()))
         loop.run_forever()
@@ -56,7 +57,8 @@ if __name__ == "__main__":
     elif mode.lower() == "client":
         print("Submission: Testing three-way handshake...")
         control = EchoControl()
-        coro = playground.getConnector('client').create_playground_connection(control.buildProtocol, remoteAddress, 101)
+        # coro = playground.getConnector('client').create_playground_connection(control.buildProtocol, remoteAddress, 101)
+        coro = playground.getConnector('lab2_protocol').create_playground_connection(control.buildProtocol, remoteAddress, 101)
         transport, protocol = loop.run_until_complete(coro)
         print("Client Connected. Starting UI t:{}. p:{}".format(transport, protocol))
         loop.run_forever()
